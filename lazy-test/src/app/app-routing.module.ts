d72466517e80e84793e220c389647ec0c6ef3f7a
Ilgam Shakirov <shilgam@gmail.com>
2023-02-01 00:41:31 +0300

    Add FirstComponent to the main route
    Steps: https://michelestieven.medium.com/lazy-loading-angular-modules-27856e940bb0

--- lazy-test/src/app/app-routing.module.ts
+++ lazy-test/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FirstComponent,
+    pathMatch: 'full' // because route is an empty
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
