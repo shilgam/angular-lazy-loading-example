5585222bcdd9b466963d70f6e517b21b16cf93a6
Ilgam Shakirov <shilgam@gmail.com>
2023-02-01 00:59:54 +0300

    Declare routes for lazy module (which displays SecondComponent)

--- lazy-test/src/app/lazy/lazy.module.ts
+++ lazy-test/src/app/lazy/lazy.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecondComponent } from './second/second.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: SecondComponent,
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [SecondComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class LazyModule { }
