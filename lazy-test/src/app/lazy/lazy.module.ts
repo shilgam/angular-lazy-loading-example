8bd9fee89ade6721ff5283ec33aaf6340ac8e77d
Ilgam Shakirov <shilgam@gmail.com>
2023-02-01 00:51:21 +0300

    $ ng generate component lazy/second
    I.e.: Create a component which will be the root component for our lazy-loaded module

--- lazy-test/src/app/lazy/lazy.module.ts
+++ lazy-test/src/app/lazy/lazy.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SecondComponent],
   imports: [
     CommonModule
   ]
